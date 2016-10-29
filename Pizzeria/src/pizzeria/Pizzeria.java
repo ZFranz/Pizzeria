e79620a376d4b2d6331016f15e0297565b316b7a
zhouzhiqiang <zhou.francesco98@gmail.com>
2016-10-29 10:38:45 +0200

    buttns

--- Pizzeria/src/pizzeria/Pizzeria.java
+++ Pizzeria/src/pizzeria/Pizzeria.java
@@ -2,6 +2,8 @@ package pizzeria;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.SWT;
 
 public class Pizzeria {
 
@@ -42,7 +44,18 @@ public class Pizzeria {
 		shell = new Shell();
 		shell.setSize(450, 300);
 		shell.setText("Pizzeria");
+		
+		Button btnNewButton = new Button(shell, SWT.NONE);
+		btnNewButton.setBounds(10, 34, 75, 25);
+		btnNewButton.setText("Apri Pizzeria");
+		
+		Button btnNewButton_1 = new Button(shell, SWT.NONE);
+		btnNewButton_1.setBounds(102, 34, 89, 25);
+		btnNewButton_1.setText("Chiudi Pizzeria");
+		
+		Button btnNewButton_2 = new Button(shell, SWT.NONE);
+		btnNewButton_2.setBounds(305, 34, 109, 25);
+		btnNewButton_2.setText("Arriva un cliente!");
 
 	}
-
 }
