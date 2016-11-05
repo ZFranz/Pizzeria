3049775ab00006bfd86a148fcc0062bd94aec2b2
zhouzhiqiang <zhouzhiqiang@Lab06_21.itseinaudi.local>
2016-11-05 12:18:37 +0100

    pizza

--- Pizzeria/src/pizzeria/Cliente.java
+++ Pizzeria/src/pizzeria/Cliente.java
@@ -12,41 +12,6 @@ public class Cliente implements Runnable {
 	}
 	
 	public void run() {
-		switch (pizzaSelezionata) {
-			case "Pizza bianca":
-				System.out.println("Pizza bianca");
-				break;
-	
-			case "Pizza rossa":
-				System.out.println("Pizza rossa");
-				break;
-				
-			case "Margherita":
-				System.out.println("Margherita");
-				break;
-				
-			case "Capricciosa":
-				System.out.println("Capricciosa");
-				break;
-				
-			case "Diavola":
-				System.out.println("Diavola");
-				break;
-				
-			case "Quattro stagioni":
-				System.out.println("Quattro stagioni");
-				break;
-				
-			case "Bufala":
-				System.out.println("Bufala");
-				break;
-				
-			case "Napoletana":
-				System.out.println("Napoletana");
-				break;
-				
-			default:
-				break;
-		}
+		lp.ordinaPizza(pizzaSelezionata);
 	}
 }
--- Pizzeria/src/pizzeria/ListaPizza.java
+++ Pizzeria/src/pizzeria/ListaPizza.java
@@ -27,7 +27,90 @@ public class ListaPizza {
 		
 	}
 	
-	public synchronized void ordinaPizza() {
-		
+	public synchronized void ordinaPizza(String pizza) {
+		switch (pizza) {
+			case "Pizza bianca":
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Pizza bianca");
+				notifyAll();
+				break;
+	
+			case "Pizza rossa":
+				try {
+					Thread.sleep(6000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Pizza rossa");
+				notifyAll();
+				break;
+				
+			case "Margherita":
+				try {
+					Thread.sleep(7000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Margherita");
+				notifyAll();
+				break;
+				
+			case "Capricciosa":
+				try {
+					Thread.sleep(10000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Capricciosa");
+				notifyAll();
+				break;
+				
+			case "Diavola":
+				try {
+					Thread.sleep(8000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Diavola");
+				notifyAll();
+				break;
+				
+			case "Quattro stagioni":
+				try {
+					Thread.sleep(11000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Quattro stagioni");
+				notifyAll();
+				break;
+				
+			case "Bufala":
+				try {
+					Thread.sleep(10000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Bufala");
+				notifyAll();
+				break;
+				
+			case "Napoletana":
+				try {
+					Thread.sleep(9000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Napoletana");
+				notifyAll();
+				break;
+				
+			default:
+				break;
+		}
 	}
 }
